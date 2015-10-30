d5f57e388366560105255b124678a5baf5502756
Pavel Garanin <mur069@yandex.ru>
2015-10-30 13:41:37 +0300

    add storege and structurs class news

---
+++ src/main/java/com/mycompany/structurs/News.java
@@ -0,0 +1,24 @@
+package com.mycompany.structurs;
+
+public class News {
+
+    public final String category;
+    public final String language;
+    public final String title;
+    public final String description;
+    public final String[] images;
+    public final String publishedDate;
+    public final String author;
+    public final String source;
+
+    public News(String category, String title, String description, String[] images, String publishedDate, String author, String source, String language) {
+        this.category = category;
+        this.title = title;
+        this.description = description;
+        this.images = images;
+        this.publishedDate = publishedDate;
+        this.author = author;
+        this.source = source;
+        this.language = language;
+    }
+}
